2eb9b95f90c5d13f6dcc2593ca4a4da29e69e636
Solomon Wakhungu <1solomonwakhungu@gmail.com>
2022-11-09 11:18:52 -0600

    Working data

--- ark-logger/main.go
+++ ark-logger/main.go
@@ -2,60 +2,77 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"net/http"
 	"time"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/guregu/dynamo"
 )
 
-type Request struct {
-	Location    string `json:"location"`
-	DeviceCount uint64 `json:"device_count"`
+type LoggerRequest struct {
+	Location    *string `json:"location"`
+	DeviceCount *int `json:"device_count"`
 }
 
-var db *dynamodb.DynamoDB
+type LoggerInfo struct {
+	CreatedAt int64
+	Location string
+	DeviceCount int `dynamo:"DeviceCount"`
+}
 
-var TableName = aws.String("Logs")
+// Response for the code
+type Response struct {
+	Response string `json:"response"`
+}
 
-func Handler(ctx context.Context, req Request) error {
+var db dynamo.Table
 
-	item, err := dynamodbattribute.MarshalMap(
-		struct {
-			Location    *string
-			CreatedAt   int64
-			DeviceCount uint64
-		}{
-			Location:    aws.String(req.Location),
-			CreatedAt:   time.Now().Unix(),
-			DeviceCount: *aws.Uint64(req.DeviceCount),
-		},
-	)
+func Handler(ctx context.Context, request events.LambdaFunctionURLRequest) (events.LambdaFunctionURLResponse, error) {
+	//log.Println(request.Body)
+	if request.RequestContext.HTTP.Method == http.MethodGet {
+		return events.LambdaFunctionURLResponse{Body: "Function is online", StatusCode: 200}, nil
+	} else if request.RequestContext.HTTP.Method == http.MethodPost{
 
-	if err != nil {
-		return err
+	var req LoggerRequest
+	err := json.Unmarshal([]byte(request.Body), &req)
+	
+	if err != nil || req.DeviceCount == nil || req.Location == nil {
+		return events.LambdaFunctionURLResponse{Body: "bad input", StatusCode: http.StatusBadRequest}, err
+	}
+	// Create dynamo struct and pass in information
+	loggerEntry := LoggerInfo{
+		CreatedAt: time.Now().Unix(),
+		Location: *req.Location,
+		DeviceCount: *req.DeviceCount,
 	}
 
-	_, err = db.PutItem(
-		&dynamodb.PutItemInput{
-			TableName: TableName,
-			Item:      item,
-		},
-	)
+	err = PutData(db, loggerEntry)
 
 	if err != nil {
-		return err
+		return events.LambdaFunctionURLResponse{Body: "Not Put data in url", StatusCode: http.StatusBadRequest}, nil
+	}
+		return events.LambdaFunctionURLResponse{Body: "Success!", StatusCode: http.StatusOK}, nil
 	}
 
-	return nil
+	
+	return events.LambdaFunctionURLResponse{Body: request.Body, StatusCode: http.StatusMethodNotAllowed}, nil
+}
+
+func PutData(table dynamo.Table, data LoggerInfo) (error) {
+	err := table.Put(data).Run()
+	return err
 }
 
 func main() {
 	sess := session.Must(session.NewSessionWithOptions(session.Options{SharedConfigState: session.SharedConfigEnable}))
-
-	db = dynamodb.New(sess)
+	tableName := "Logs"
+	config := &aws.Config{Region: aws.String("us-east-1")}
+	db = dynamo.New(sess, config).Table(tableName)
+	//db = dynamodb.New(sess)
 
 	lambda.Start(Handler)
 }
