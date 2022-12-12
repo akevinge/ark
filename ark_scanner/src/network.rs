0315381afdb1e96b1627ea419f8c8570fd750d51
Kevin G <ge.kevin.a@gmail.com>
2022-12-12 12:06:06 -0600

    Add Rust CI
    chore: trigger workflow
    
    chore: Update rs workflow
    
    chore: trigger workflow
    
    chore: update workflows
    
    Add AWS deployment workflow
    
    fix: AWS deploy workflow
    
    fix: AWS deploy syntax
    
    fix: AWS deploy syntax
    
    fix: workflow
    
    fix: workflow remove working-directory
    
    Add workflow_dispatch for manual runs
    
    fix: Run zig install with sudo
    
    fix: Change AWS deploy name
    
    fix: remove 'v' from cargo lambda release tag
    
    fix: remove version tag from cargo lambda
    
    fix: Switch to cargo build of cargo-lambda
    
    fix: cargo-lambda install
    
    Add API key config
    
    Toggle off sam deploy confirmation

--- ark_scanner/src/network.rs
+++ ark_scanner/src/network.rs
@@ -11,10 +11,7 @@ use std::{
 use log::log;
 use pnet::{
     packet::{
-        arp::{
-            ArpHardwareTypes::{self},
-            ArpOperations, MutableArpPacket,
-        },
+        arp::{ArpHardwareTypes, ArpOperations, MutableArpPacket},
         ethernet::{EtherTypes, MutableEthernetPacket},
         Packet,
     },
@@ -108,7 +105,7 @@ pub fn compute_subnet_ips(sample_up: Ipv4Addr, subnet_mask: Ipv4Addr) -> Vec<Ipv
 
     let mut out: Vec<Ipv4Addr> = vec![];
 
-    // ignore broadcast network
+    // skip broadcast network
     for raw in (first + 1)..last {
         out.push(Ipv4Addr::new(
             (raw >> 24) as u8,
